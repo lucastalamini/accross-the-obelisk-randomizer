179d67ba39cf9255c977488a0c66481322139303
lucastalamini <lucastalamini@hotmail.com>
2022-08-20 03:40:27 -0300

    first commit

---
+++ randomizer-form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>Randomizer</title>
+</head>
+
+<body>
+  <div id="main-container">
+    <form id="form-container" action="randomizer-processor.php" method="post">
+      <input class="flex-item" type="text" name="nameOne" value="Be">
+      <input class="flex-item" type="text" name="nameTwo" value="Enzo">
+      <input class="flex-item" type="text" name="nameThree" value="Lucas">
+      <input class="flex-item" type="text" name="nameFour" value="Luiz">
+      <button id="btn" type="submit" name="submit">Randomize!</button>
+    </form>
+  </div>
+</body>
+
+</html>
---
+++ randomizer-processor.php
@@ -0,0 +1,29 @@
+<style>
+  <?php include 'C:\Users\lucas\Documents\xampp\htdocs\accross-the-obelisk-randomizer\style.css'; ?>* {
+    background-color: #000000
+  }
+</style>
+
+<?php
+
+if (isset($_POST['submit'])) {
+
+  $nameOne = $_POST['nameOne'];
+  $nameTwo = $_POST['nameTwo'];
+  $nameThree = $_POST['nameThree'];
+  $nameFour = $_POST['nameFour'];
+
+  $names = array($nameOne, $nameTwo, $nameThree, $nameFour);
+
+  $classes = array("Mage 🧙‍♂️", "Healer 🌿", "Thief 🏹", "Tank 🛡️");
+
+  shuffle($classes);
+
+  $randomizedArray = array_rand($classes, 4);
+
+  array_map(function ($name, $class) {
+    echo "<div id=\"php-container\"><p id=\"php-item\">$name will play as the $class" . "<br>" . "<br> </p></div>";
+  }, $names, $classes);
+} else {
+  echo "Fail loser lol L.";
+}
