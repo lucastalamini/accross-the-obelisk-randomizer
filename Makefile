8426fd732adab2c663f7d977cb71444af7039cd4
charlesatformassembly <84788484+charlesatformassembly@users.noreply.github.com>
2022-08-21 18:04:41 -0300

    Improve styling. (#2)

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
+# Initializes a Docker container with a XAMPP server where the web page is hosted.
 xampp:
 	docker run --name myXampp -p 41061:22 -p 41062:80 -d -v $(shell pwd):/www tomsik68/xampp:8
--- randomizer-processor.php
+++ randomizer-processor.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="style.css"/>
 
 <?php
 
@@ -6,33 +6,38 @@
 
 if (isset($_POST['submit'])) {
 
-  // Assign the value of each form field to a variable
+    // Assign the value of each form field to a variable
 
-  $nameOne = $_POST['nameOne'];
-  $nameTwo = $_POST['nameTwo'];
-  $nameThree = $_POST['nameThree'];
-  $nameFour = $_POST['nameFour'];
+    $nameOne = $_POST['nameOne'];
+    $nameTwo = $_POST['nameTwo'];
+    $nameThree = $_POST['nameThree'];
+    $nameFour = $_POST['nameFour'];
 
-  // Create an array using the $name variables
+    // Create an array using the $name variables
 
-  $names = array($nameOne, $nameTwo, $nameThree, $nameFour);
+    $names = [$nameOne, $nameTwo, $nameThree, $nameFour];
 
-  // Array containing each of the available classes
+    // Array containing each of the available classes
 
-  $classes = array("Mage 🧙‍♂️", "Healer 🌿", "Thief 🏹", "Tank 🛡️");
+    $classes = ["Mage 🧙‍♂️", "Healer 🌿", "Thief 🏹", "Tank 🛡️"];
 
-  // Randomize the array containing the available classes
+    // Randomize the array containing the available classes
 
-  shuffle($classes);
+    shuffle($classes);
 
-  // Map through the randomized classes array while assigning each class to one of the values on the $names array
-
-  array_map(function ($name, $class) {
-    echo "<div id=\"php-container\"><p id=\"php-item\">$name will play as the $class" . "<br>" . "<br> </p></div>";
-  }, $names, $classes);
+    // Map through the randomized classes array while assigning each class to one of the values on the $names array
+    echo "<div id=\"main-container\"><div id=\"php-container\">";
+    array_map(
+        static function ($name, $class) {
+            echo "<text id=\"php-item\">$name will play as the $class</text>";
+        },
+        $names,
+        $classes
+    );
+    echo "</div></div>";
 } else {
 
-  // Error message in case the randomizer-processor.php is accessed directly
+    // Error message in case the randomizer-processor.php is accessed directly
 
-  echo "Please submit the form.";
+    echo "Please submit the form.";
 }
